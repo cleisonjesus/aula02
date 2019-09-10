a46657df7b350afe3a27fe69dda17c350bff6638
cleisonjesus <ieqleaodejuda@hotmail.com>
2019-09-10 20:25:56 -0300

    second commit

---
+++ src/com/company/exercicio01-A).java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class exercicio03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nome;
+        System.out.println("Informe Seu Nome");
+        nome = sc.next();
+        int idade;
+                System.out.println("informe sua idade");
+                        idade = sc.nextInt();
+            }
+            }
+
+
---
+++ src/com/company/exercicio01=B).java
@@ -0,0 +1,13 @@
+
+
+package com.company;
+
+public class exercicio{
+    public static void main(String[] args){
+
+    }
+}
+
+
+
+
